fbce2478831c2769a38d55b0b39b92340e6e5e31
21120535 <98611090+quynluvpinky@users.noreply.github.com>
2024-10-09 21:42:04 +0700

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter basename="/tic-tac-toe">
+      <Routes>
+        <Route path="/" element={<App />}></Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
